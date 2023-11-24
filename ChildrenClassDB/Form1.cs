de070b0da5634857b443505395f582da395e5c71
PavelSadykov <2439747@gmail.com>
2023-11-24 14:07:41 +0500

    Добавьте файлы проекта.

---
+++ ChildrenClassDB/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+using System.Data.SQLite;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace ChildrenClassDB
+{
+    public partial class Form1 : Form
+    {
+        string filename;
+        bool chooseDB = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DBJob db;
+        string textTable;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (chooseDB)
+            {
+                MessageBox.Show($"Соединение с базой {filename}");
+                filename = openFileDialog1.FileName;
+                db.Conn.Open();
+                
+            }
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+                db = new SQLiteDBJob(filename);
+                chooseDB = true;
+            }
+            string sql = "Select * from sqlite_master";
+           // string sql = "SELECT name FROM sqlite_master WHERE type='tableName'";
+            DataTable DtProduct = new DataTable();
+            DbDataAdapter adProduct = db.GetDataAdapter(sql);
+            adProduct.Fill(DtProduct);
+            dataGridView1.DataSource=DtProduct;
+        
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            db = new SQLDBJob("");
+
+            int numberOfPopularBooks = 3; // 3 самые популярные книги
+
+            //  SQL-запрос 
+            string sql = $"SELECT TOP {numberOfPopularBooks}  Title, TotalCopies - CopiesInLibrary As MostPopular\r\nFROM Books\r\nORDER BY MostPopular DESC;";
+          
+            DataTable DtProduct = new DataTable();
+            DbDataAdapter adProduct = db.GetDataAdapter(sql);
+            adProduct.Fill(DtProduct);
+            dataGridView1.DataSource = DtProduct;
+         
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!button1.Enabled)
+            {
+                textTable = textBox1.Text;
+                bool answer = db.HasTable(textTable);
+                MessageBox.Show($"Указанная таблица = {answer}");
+            }
+            else
+            {
+                MessageBox.Show("Сначала нажмите кнопку 'Коннект SQLite'");
+            }
+          
+
+            }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!button6.Enabled)
+            {
+                textTable = textBox1.Text;
+                bool answer = db.HasTable(textTable);
+                MessageBox.Show($"Указанная таблица = {answer}");
+            }
+            else
+            {
+                MessageBox.Show("Сначала нажмите кнопку 'Коннект SQL'");
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string ConnString = @"Data Source=DESKTOP-EKD1ADK;Initial Catalog=Library;Integrated Security=True";
+            db = new SQLDBJob(ConnString);
+            db.Conn.Open();
+            MessageBox.Show("Соединение с базой установлено");
+
+        }
+    }
+    }
+
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+using System.Data.Entity.Infrastructure;
+
+namespace ClassLibrary1
+{
+    public abstract class DBJob
+    {
+
+        public DbConnection Conn;
+        public DbDataAdapter DataAdapter;
+        public string ConnString { get; set; }
+        public abstract DbConnection GetConnection();
+        public abstract DbDataAdapter GetDataAdapter(string sql);
+        public abstract bool HasTable(string tableName);// запрос имени таблицы для проверки наличия ее
+    }
+
+
+    public class SQLiteDBJob : DBJob
+    {
+        public SQLiteConnection _conn;
+        private SQLiteDataAdapter _adapter;
+        public SQLiteDBJob(string constr)
+        {
+            ConnString = "DataSource=" + constr;
+            _conn = new SQLiteConnection(ConnString);
+            Conn = _conn;
+        }
+
+        public override DbConnection GetConnection()
+        {
+            _conn = new SQLiteConnection(ConnString);
+            Conn = _conn;
+            return Conn;
+        }
+        public override bool HasTable(string tableName)
+        {
+                string checkTableQuery = $"SELECT name FROM sqlite_master WHERE type='table' AND name='{tableName}'";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(checkTableQuery, (SQLiteConnection)Conn))
+                {
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        return true;
+                  
+                }
+                    else
+                    {
+                        return false;
+                    }
+               
+            }
+           
+        }
+
+        public override DbDataAdapter GetDataAdapter (string sql)
+        {
+          
+            _adapter = new SQLiteDataAdapter(sql, _conn);
+            DataAdapter = _adapter;
+            return DataAdapter;
+        }
+    }
+
+    public class SQLDBJob : DBJob
+    {
+        public SqlConnection _conn;
+        private SqlDataAdapter _adapter;
+        public SQLDBJob(string constr)
+        {
+            ConnString = @"Data Source=DESKTOP-EKD1ADK;Initial Catalog=Library;Integrated Security=True"; ;
+            _conn = new SqlConnection (ConnString);
+            Conn = _conn;
+        }
+        public override bool HasTable(string tableName)
+        {
+
+            string checkTableQuery = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = '{tableName}';";
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                using (SqlCommand command = new SqlCommand(checkTableQuery, conn))
+                {
+                   
+                        conn.Open();
+                        int tableCount = Convert.ToInt32(command.ExecuteScalar());
+                        if (tableCount > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    
+                }
+            }
+
+            
+        }
+        public override DbConnection GetConnection()
+        {
+            _conn = new SqlConnection(ConnString);
+            Conn = _conn;
+            return Conn;
+        }
+        public override DbDataAdapter GetDataAdapter(string sql)
+        {
+            _adapter = new SqlDataAdapter(sql, _conn);
+            DataAdapter = _adapter;
+            return DataAdapter;
+        }
+    }
+
+
+
+}
